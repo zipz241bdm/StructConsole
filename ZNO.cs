fb9ec7b7e1ef7ec7707a160c3e8ebfbc90b3b5c1
Dmytro <bilousd61@gmail.com>
2026-03-10 16:58:40 +0100

    extract ZNO and Entrant structs into separate files

---
+++ ZNO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructConsole
+{
+    struct ZNO
+    {
+        public string Subject;
+        public int Points;
+
+        public ZNO(string subject, int points)
+        {
+            Subject = subject;
+            Points = points;
+        }
+    }
+}
